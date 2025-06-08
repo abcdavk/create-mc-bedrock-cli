a1650a1db2e55a502d14bdc29bf9cab63a112716
Keyyard <keyyard8888@gmail.com>
2025-06-08 16:50:37 +0700

    refactor readme

--- src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs/promises';
-
-export async function cleanupTempFiles() {
-  try {
-    await fs.rm('./temp-repo', { recursive: true, force: true });
-  } catch (error) {
-    console.error('Error cleaning up temporary files:', error.message);
-  }
-}
